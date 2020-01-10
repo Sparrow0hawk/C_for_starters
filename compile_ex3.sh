166b324c6f79e40a0044b9e82a8927ebd48b44ab
sparrow0hawk <alexjcoleman@gmail.com>
2020-01-10 21:42:28 +0000

    ex3 formatted printing

---
+++ compile_ex3.sh
@@ -0,0 +1,10 @@
+# 2019-01-10 Alex Coleman
+# C_for_starters ex1
+# compile ex1.c with makefile
+make ex3
+
+# move compiled file into bin
+mv ex3 bin/
+
+# run compiled file
+bin/ex3
---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("You are %d inches tall.\n", height);
+
+    return 0;
+}
