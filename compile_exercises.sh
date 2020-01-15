9867acdb781b7a59f7dc2ed33f0885e2afe59a35
sparrow0hawk <alexjcoleman@gmail.com>
2020-01-15 21:41:55 +0000

    updated compilation shell script

---
+++ compile_exercises.sh
@@ -0,0 +1,13 @@
+# 2019-01-10 Alex Coleman
+# C_for_starters ex7
+# compile ex7.c with makefile
+
+exfile=$1
+make $exfile
+
+chmod +x $exfile
+# move compiled file into bin
+mv $exfile bin/
+
+# run compiled file
+bin/$exfile
